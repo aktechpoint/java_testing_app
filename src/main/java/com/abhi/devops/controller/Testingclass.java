8d79dfa22623ab542c767068abc0dc45fcf54352
abhishek kumar <aktechuniverse@gmail.com>
2025-07-10 21:41:32 +0530

    first commit

---
+++ src/main/java/com/abhi/devops/controller/Testingclass.java
@@ -0,0 +1,12 @@
+package com.abhi.devops.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Testingclass {
+	
+	public String devTesting() {
+		return "hi we are learning java deployment process with java and ubuntu on aws ";
+	}
+
+}
