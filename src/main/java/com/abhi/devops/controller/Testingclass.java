c2f0997741153e79dd44133c6d2da14da826bf9d
Abhishek singh <aktechpoint62@gmail.com>
2026-04-19 22:39:32 +0530

    Update Testingclass.java

--- src/main/java/com/abhi/devops/controller/Testingclass.java
+++ src/main/java/com/abhi/devops/controller/Testingclass.java
@@ -2,6 +2,7 @@ package com.abhi.devops.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;   // ✅ Add this line
 
 @RestController
 public class Testingclass {
@@ -10,9 +11,9 @@ public class Testingclass {
 	public String devTesting() {
 		return "hi we are learning java deployment process with java and ubuntu on aws !";
 	}
+
 	@GetMapping("/health")
     public ResponseEntity<String> healthCheck() {
-        return ResponseEntity.ok("helat is working");
+        return ResponseEntity.ok("health is working");
     }
-
 }
